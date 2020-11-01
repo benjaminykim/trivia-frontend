98dc65246e3183675c06d14af28528b2e2bca514
Benjamin Kim <benkim96@gmail.com>
2020-10-31 21:35:21 -0700

    [dep] add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+			<body>
+				<Button></Button>
+			</body>
     </div>
   );
 }
