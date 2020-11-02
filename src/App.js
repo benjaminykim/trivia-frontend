f051c7e0bd8137cc17e57b983a21f5abc40388c3
Benjamin Kim <benkim96@gmail.com>
2020-11-01 19:37:10 -0800

    add routing and navbar

--- src/App.js
+++ src/App.js
@@ -2,41 +2,35 @@ import './App.css';
 import { Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Trivia from './components/Trivia';
 import Home from './components/Home';
+import LeaderBoard from './components/LeaderBoard.js';
 
 function App() {
-	const [session, setSession] = useState(false);
-	const [questions, setQuestions] = useState([]);
 
-	useEffect(() => {
+	return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/leaderboard">LeaderBoard</Link>
+            </li>
+          </ul>
+        </nav>
 
-	});
-
-	async function startTrivia() {
-		console.log("Start Trivia Fetch");
-		await axios.get("https://dev.briefs.link/trivia")
-				.then(response => {
-					console.log(response);
-					setQuestions(response.data);
-					setSession(true);
-		});
-	}
-
-  return (
-		<Container className="main">
-			<div className="main-header">Tandem Trivia!</div>
-			{(session)
-			? <Trivia questions={questions}></Trivia>
-			: <Button
-					variant="outline-primary"
-					onClick={startTrivia}
-				>
-					Start Trivia!
-				</Button>
-			}
-		</Container>
+        <Switch>
+          <Route path="/leaderboard">
+            <LeaderBoard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
