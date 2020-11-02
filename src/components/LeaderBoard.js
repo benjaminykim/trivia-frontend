bf944b68f016acfbd775e186d737a0557dbbc683
Benjamin Kim <benkim96@gmail.com>
2020-11-01 20:07:10 -0800

    working checkpoint with leaderboard

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,21 +1,53 @@
 import '../App.css';
-import { Container } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {React} from 'react';
+import {React, useState, useEffect } from 'react';
 import axios from 'axios';
 
 function LeaderBoard() {
-	async function startTrivia() {
-		console.log("Start Trivia Fetch");
-		await axios.get("https://dev.briefs.link/trivia")
-				.then(response => {
-					console.log(response);
-		});
-	}
+	const [scores, setScores] = useState([]);
+
+	useEffect(async () => {
+		const result = await axios('https://dev.briefs.link/score');
+		setScores(result.data);
+		console.log(result);
+		console.log(scores);
+	}, []);
+
+	const tIndex = {
+    fontSize: '2rem',
+    color: 'white',
+  };
+
+	const tEntry = {
+    fontSize: '1.2rem',
+    color: 'white',
+  };
 
   return (
 		<Container className="main">
-			<div className="main-header">Tandem Trivia!</div>
+			<div className="main-header">LeaderBoard</div>
+			<div className="score-table">
+				<Table responsive bordered hover variant="dark">
+					<thead>
+						<tr>
+							<td style={tIndex}>Username</td>
+							<td style={tIndex}>Score</td>
+						</tr>
+					</thead>
+					<tbody>
+						{scores.map((record) => {
+							return (
+								<tr>
+									<td style={tEntry}>{record.user}</td>
+									<td style={tEntry}>{record.score}</td>
+								</tr>
+							);
+						}
+						)}
+					</tbody>
+				</Table>
+			</div>
 		</Container>
   );
 }
